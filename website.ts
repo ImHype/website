0b8f2495f218749b8450dc4970adc9cdb7f229f2
xujunyu.joey <xujunyu.joey@bytedance.com>
2021-10-18 10:50:34 +0800

    feat: add helloworld

--- website.ts
+++ website.ts
@@ -1,25 +1,41 @@
-import { listenAndServe } from "https://deno.land/std/http/server.ts";
+import { listenAndServe } from "https://deno.land/std@0.111.0/http/server.ts";
+
+async function handleRequest(request: Request): Promise<Response> {
+  const { pathname } = new URL(request.url);
+
+  // This is how the server works:
+  // 1. A request comes in for a specific asset.
+  // 2. We read the asset from the file system.
+  // 3. We send the asset back to the client.
+
+  // Check if the request is for style.css.
+  if (pathname.startsWith("/style.css")) {
+    // Read the style.css file from the file system.
+    const file = await Deno.readFile("./style.css");
+    // Respond to the request with the style.css file.
+    return new Response(file, {
+      headers: {
+        "content-type": "text/css",
+      },
+    });
+  }
+
+  return new Response(
+    `<html>
+      <head>
+        <link rel="stylesheet" href="style.css" />
+      </head>
+      <body>
+        <h1>Example</h1>
+      </body>
+    </html>`,
+    {
+      headers: {
+        "content-type": "text/html; charset=utf-8",
+      },
+    },
+  );
+}
 
 console.log("Listening on http://localhost:8080");
-
-
-
-const text = `
-<html>
-  <title>Joey</title>
-  <head>
-    <style>
-      .h1 { backround: green}
-    </style>
-  </head>
-  <body>
-    <h1>Joey Website</h1>
-  </body>
-</html>
-`;
-
-await listenAndServe(":8080", (_req) => {
-  return new Response(text, {
-    headers: { "content-type": "text/html" },
-  });
-});
+await listenAndServe(":8080", handleRequest);
