eb882909db5d296072834b989733d36e2a19ffa4
xujunyu.joey <xujunyu.joey@bytedance.com>
2021-10-18 10:38:15 +0800

    feat: add helloworld

--- website.ts
+++ website.ts
@@ -1,6 +1,23 @@
 import { listenAndServe } from "https://deno.land/std/http/server.ts";
 
 console.log("Listening on http://localhost:8080");
+
+
+
+const text = `
+<html>
+  <title>Joey</title>
+  <head>
+    <style>
+      .h1 { backround: green}
+    </style>
+  </head>
+  <body>
+    <h1>Joey Website</h1>
+  </body>
+</html>
+`;
+
 await listenAndServe(":8080", (_req) => {
   return new Response("Hello World!", {
     headers: { "content-type": "text/plain" },
